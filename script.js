bf99ebf6c75f99bd941fc1d2fa6e7e4da9b4421c
jhonihz30 <jhonihz30@gmail.com>
2025-10-21 16:29:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ function pedir() {
     let mensaje = "¡Hola!, queria pedir " + selection + ", para la dirección: " + direccion + "."
     window.location.href = "https://wa.me/59894850345?text=" + mensaje
 }
+
+function toggle_info(button) {
+    image_container = button.nextElementSibling;
+    if (image_container.classList.contains('active')) 
+    {
+        image_container.classList.remove('active');
+        button.classList.remove("button_out")
+    }
+    else {
+        document.querySelectorAll('.active').forEach(element => {
+            element.classList.remove('active')
+            element.previousElementSibling.classList.remove('button_out')
+        });
+        image_container.classList.add('active');
+        button.classList.add("button_out")
+    }
+
+    
+}
