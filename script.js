8183fda506effe7eae5b0a7234c5cdcf3df033f8
jhonihz30 <jhonihz30@gmail.com>
2025-10-21 19:25:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 let selection = null
 let direccion = null
 
+fetch('catalogo.txt')
+    .then(response => response.text())
+    .then(csvText => {
+        const lines = csvText.trim().split('\n');
+        const headers = lines[0].split(';').map(h => h.replace(/"/g,"").trim());
+        const data = lines.slice(1).map(line => {
+            const values = line.trim().split(';').map(h => h.replace(/"/g,"").trim());
+            const obj = {}
+            headers.forEach((header, i) => {
+                obj[header] = values[i];
+            });
+            return obj
+        })
+
+        data.reverse()
+        const container = document.getElementById('section');
+        data.forEach(postre => {
+            const card = document.createElement('div');
+            card.classList.add('element');
+            card.innerHTML = `
+            <img class="star" src="images/star.png" alt="">
+            <a class="name">${postre.Nombre}</a>
+            <p class="name detail">${postre.Detalle}</p>
+            <button onclick="toggle_info(this)" class="info_button" aria-details="Alternar Descripción">
+                <img src="images/elements/eye.png" alt="">
+            </button>
+            <div class="image_container">
+                <img class="picture" src="images/Catalogo/${postre.Imagen}.jpg" alt="">
+                <div class="info">
+                <p>${postre.Descripcion}</p>
+                </div>
+            </div>
+            <a class="price">${postre.Precio}</a>
+            <button class="ped" onclick= "refreshSel('${postre.MSG}')">Pedir</button>`
+            container.appendChild(card)
+        })
+    });
+
+
 function refreshSel(value) {
     selection = value;
     console.log("Seleccionaste " ,selection)
